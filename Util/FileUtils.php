979c198debd893e8b5a0c8eb48133f6e5debef8d
nilov <nilov@glavweb.ru>
2023-05-10 00:49:05 +0700

    Another fix bug with method FileUtils::basename().
    (cherry picked from commit 28309cad8a2665a05b46ce442ad11516972a811a)

--- Util/FileUtils.php
+++ Util/FileUtils.php
@@ -160,6 +160,6 @@ class FileUtils
      */
     public static function basename(string $fileName): string
     {
-        return substr(strrchr(rawurldecode(basename(rawurlencode($fileName))), DIRECTORY_SEPARATOR), 1);
+        return substr(strrchr($fileName, DIRECTORY_SEPARATOR), 1) ?: $fileName;
     }
 }
